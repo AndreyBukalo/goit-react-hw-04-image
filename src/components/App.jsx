ea73bb486059da0f1f59433dee00b4b0227fa891
armagedd <bukaloandrey@gmail.com>
2022-08-17 10:12:23 +0300

    start

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Box } from './Box';
+
+export class App extends Component {
+  state = {
+    
+  };
+
+  
+  render() {
+    return (
+      <Box width={380} listStyle="none" ml="45px" mt="20px" p="0">
+       
+        />
+      </Box>
+    );
+  }
+}
